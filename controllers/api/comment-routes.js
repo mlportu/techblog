006021d918f6dc31a8fd4fa0554d2fe173ac6db4
Michael Portu <michaelportu@Michaels-MacBook-Pro.local>
2020-09-06 16:55:15 -0500

    comments can be deleted from posts

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -28,4 +28,23 @@ router.post('/', (req, res) => {
     // }
 });
 
+router.delete('/:id', (req, res) => {
+    Comment.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbCommentData => {
+        if(!dbCommentData){
+            res.status(400).json({message: 'No comment found with this id'});
+            return;
+        }
+        res.json(dbCommentData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
 module.exports = router;
