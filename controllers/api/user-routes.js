f30c73281c29b6d2b649fde88ad8b3440d5e2d27
Michael Portu <michaelportu@Michaels-MacBook-Pro.local>
2020-09-06 22:13:42 -0500

    login, sign up and log out functionality added

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -60,17 +60,18 @@ router.get('/:id', (req, res) => {
 router.post('/', (req, res) => {
     // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
     User.create({
-        username:req.body.username,
+        username: req.body.username,
         password: req.body.password
     })
         .then(dbUserData => {
-            // req.session.save(()=>{
-            //     req.session.use_id = dbUserData.id;
-            //     req.session.username = dbUserData.username;
-            //     req.session.loggedIn = true;
+          
+            req.session.save(()=>{
+                req.session.use_id = dbUserData.id;
+                req.session.username = dbUserData.username;
+                req.session.loggedIn = true;
 
                 res.json(dbUserData);
-            // });
+            });
         })
         .catch(err => {
             console.log(err);
@@ -85,29 +86,28 @@ router.post('/login', (req, res) => {
             username:req.body.username
         }
     }).then(dbUserData => {
-        if(!dbUserData) {
-            res.status(400).json({message: 'No user with that username'});
-            return;
-        }
-
-    //verify user
-    const validPassword = dbUserData.checkPassword(req.body.password);
-
-    if(!validPassword) {
-        res.status(400).json({message: 'Incorrect Password'});
-        return;
+        if (!dbUserData) {
+          res.status(400).json({ message: 'No user with that email address!' });
+          return;
         }
     
-    // req.session.save(() => {
-    //     //declare session variables
-    //     req.session.user_id = dbUserData.id;
-    //     req.session.username = dbUserData.username;
-    //     req.session.loggedIn = true;
-
-        res.json({user:dbUserData, message: 'Your are now logged in!'});
+        const validPassword = dbUserData.checkPassword(req.body.password);
+    
+        if (!validPassword) {
+          res.status(400).json({ message: 'Incorrect password!' });
+          return;
+        }
+        
+        req.session.save(()=>{
+            // declare session variables
+            req.session.use_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+    
+          res.json({ user: dbUserData, message: 'You are now logged in!' });
         });
-    // });
-});
+      });
+    });
 
 //logout
 router.post('/logout', (req, res) => {
