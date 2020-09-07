f60a3d59589d32238685b628f562081c17de533d
Michael Portu <michaelportu@Michaels-MacBook-Pro.local>
2020-09-07 14:41:51 -0500

    session sensative login/logout fixed

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,6 +3,7 @@ const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 
 router.get('/', (req, res) => {
+    console.log(req.session);
     Post.findAll({
         attributes: [
           'id',
@@ -30,7 +31,7 @@ router.get('/', (req, res) => {
           const posts = dbPostData.map(post => post.get({ plain: true }));
           res.render('homepage', {
               posts,
-              
+              loggedIn: req.session.loggedIn
           });
       })
       .catch(err => {
